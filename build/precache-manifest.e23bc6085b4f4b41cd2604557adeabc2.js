87a07aca058b3aee48730f696dd625f0235e877b
NishchayWankar <71606731+Nishchay9518@users.noreply.github.com>
2021-05-11 10:51:08 +0530

    initial commit

---
+++ build/precache-manifest.e23bc6085b4f4b41cd2604557adeabc2.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d01cb5857a2a562061fd1144b6c7a492",
+    "url": "/index.html"
+  },
+  {
+    "revision": "ed5874af5ddd9d27c8fd",
+    "url": "/static/css/2.7c35e809.chunk.css"
+  },
+  {
+    "revision": "df91ffb394c8a3f9a29c",
+    "url": "/static/css/main.02729728.chunk.css"
+  },
+  {
+    "revision": "ed5874af5ddd9d27c8fd",
+    "url": "/static/js/2.ad8e3933.chunk.js"
+  },
+  {
+    "revision": "68eb484dce045119a52132a8eb902e52",
+    "url": "/static/js/2.ad8e3933.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "df91ffb394c8a3f9a29c",
+    "url": "/static/js/main.1058a9cc.chunk.js"
+  },
+  {
+    "revision": "3ff5d6fb19a2b7a4acf2",
+    "url": "/static/js/runtime-main.8b39ddf8.js"
+  }
+]);
